2025f134743a4467b1abbecdc8e0489d16d54823
EdanFlame <237397488@qq.com>
2021-07-27 14:29:01 +0800

    init structure

--- script/run.py
+++ script/run.py
@@ -1,17 +1,16 @@
-from tradeServer import WebEngine
+import subprocess
 
 from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
-
 from vnpy.gateway.ctp import CtpGateway
-from vnpy.app.cta_strategy import CtaStrategyApp
-from vnpy.app.cta_backtester import CtaBacktesterApp
+
+from vnpy_webtrader.tradeServer import WebEngine
+from vnpy_webtrader.webServer import main
 
 event_engine = EventEngine()
 main_engine = MainEngine(event_engine)
-
 main_engine.add_gateway(CtpGateway)
 
-c = WebEngine(main_engine, event_engine)
-
-print('启动交易服务器进程')
+trade = WebEngine(main_engine, event_engine)
+print('交易服务器启动完成')
+web = subprocess.run(main())
--- vnpy_webtrader/webServer.py
+++ vnpy_webtrader/webServer.py
@@ -1,29 +1,31 @@
-from datetime import datetime
-from time import sleep
+
 from typing import Any, List
 import json
 import asyncio
-import threading
+import datetime
 
-import websockets
-
-import uvicorn
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Path, Query
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException, status, Query, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from jose import jwt
+import uvicorn
 
 from vnpy.rpc import RpcClient
-from vnpy.trader.object import OrderRequest, CancelRequest, SubscribeRequest
-from vnpy.trader.object import Exchange, Direction, OrderType, CancelRequest, Offset
+from vnpy.trader.object import OrderRequest, CancelRequest
+from vnpy.trader.object import Exchange, Direction, OrderType, Offset
+
 
 REQ_ADDRESS = 'tcp://127.0.0.1:2014'
 SUB_ADDRESS = 'tcp://127.0.0.1:4102'
 
-# 创建fastapi对象
-app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 # 创建RPC对象
 rpc = RpcClient()
+
+
 # 创建websocket管理对象
 class ConnectionManager:
     def __init__(self):
@@ -48,6 +50,23 @@ manager = ConnectionManager()
 
 DD = ""
 
+TODAY = str(datetime.datetime.now().date())
+
+# 读取web登录信息
+with open("vnpy_webtrader/Web_setting.json", "r", encoding="utf8") as f:
+    WEBSETTING = json.load(f)
+    USERNAME = WEBSETTING["username"]
+    PASSWORD = WEBSETTING["password"]
+    TOKEN = jwt.encode({USERNAME: PASSWORD}, TODAY, algorithm="HS256")
+
+# 读取ctp登录信息
+with open("vnpy_webtrader/CTP_connect.json", "r", encoding="utf8") as f:
+    CTPSETTING = json.load(f)
+
+
+# 创建fastapi对象
+app = FastAPI()
+
 # 允许所有跨域请求
 app.add_middleware(
     CORSMiddleware,
@@ -57,8 +76,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-with open("./index.html") as f:
+
+# 获取静态页面
+with open("static/index.html") as f:
     index = f.read()
+
 
 # 主页面
 @app.get("/")
@@ -66,50 +88,37 @@ async def get():
     """获取主页面"""
     return HTMLResponse(index)
 
-# 连接
+
+# token验证
+@app.post("/token")
+async def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    token = jwt.encode({form_data.username: form_data.password}, TODAY, algorithm="HS256")
+    if token != TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return {"access_token": token, "token_type": "bearer"}
+
+
+# 连接交易服务器
 class Setting(BaseModel):
     userid: str
-    password: str 
-    brokerid: str 
+    password: str
+    brokerid: str
     td_address: str
-    md_address: str 
-    appid: str 
+    md_address: str
+    appid: str
     auth_code: str
 
 
 @app.get("/connect")
-async def get_connect():
-    """"""
-    setting = {
-    "用户名": "158995",
-    "密码": "#Hrnmm1234",
-    "经纪商代码": "9999",
-    "交易服务器": "180.168.146.187:10130",
-    "行情服务器": "180.168.146.187:10131",
-    "产品名称": "simnow_client_test",
-    "授权编码": "0000000000000000"
-    }
-    tmp = rpc.connect(setting, "CTP")
+async def post_connect(token: str = Depends(oauth2_scheme)):
+    """连接"""
+    tmp = rpc.connect(CTPSETTING, "CTP")
     return ("return: ", tmp)
 
-    # "用户名": "189672",
-    # "密码": "#gyf123456",
-    
-
-@app.post("/connect")
-async def post_connect(setting: Setting):
-    """"""
-    tmp = {
-        "用户名": setting.userid,
-        "密码": setting.password,
-        "经纪商代码": setting.brokerid,
-        "交易服务器": setting.td_address,
-        "行情服务器": setting.md_address,
-        "产品名称": setting.appid,
-        "授权编码": setting.auth_code
-    }
-    tmp = rpc.connect(tmp, "CTP")
-    return ("return: ", tmp)
 
 # 发送订单
 class Order(BaseModel):
@@ -124,7 +133,7 @@ class Order(BaseModel):
 
 
 @app.get("/send_order")
-async def get_send_order():
+async def get_send_order(token: str = Depends(oauth2_scheme)):
     """"""
     data = OrderRequest("60000", Exchange("SHFE"), Direction("多"), OrderType("询价"), 100.0)
     tmp = rpc.send_order(data, "CTP")
@@ -132,7 +141,7 @@ async def get_send_order():
 
 
 @app.post("/send_order")
-async def post_send_order(order:Order):
+async def post_send_order(order: Order, token: str = Depends(oauth2_scheme)):
     """"""
     data = OrderRequest(
         order.symbol,
@@ -156,14 +165,15 @@ class CRequest(BaseModel):
 
 
 @app.get("/cancel_order")
-async def get_cannel_order():
+async def get_cannel_order(token: str = Depends(oauth2_scheme)):
     """"""
     data = CancelRequest("3_-1041318151_2", "60000",  Exchange("SHFE"))
     tmp = rpc.cancel_order(data, "CTP")
     return ("return: ", tmp)
 
+
 @app.post("/cancel_order")
-async def post_cannel_order(cq:CRequest):
+async def post_cannel_order(cq: CRequest, token: str = Depends(oauth2_scheme)):
     """"""
     data = CancelRequest(cq.orderid, cq.symbol, cq.exchange)
     tmp = rpc.cancel_order(data, "CTP")
@@ -172,7 +182,7 @@ async def post_cannel_order(cq:CRequest):
 
 # 查询订单
 @app.get("/query_order")
-async def get_query_order():
+async def get_query_order(token: str = Depends(oauth2_scheme)):
     """"""
     tmp = rpc.get_order("aaa")
     return ("return: ", tmp)
@@ -180,7 +190,7 @@ async def get_query_order():
 
 # 查询交易
 @app.get("/trade")
-async def get_trade():
+async def get_trade(token: str = Depends(oauth2_scheme)):
     """"""
     tmp = rpc.get_trade("aaa")
     return ("return: ", tmp)
@@ -188,7 +198,7 @@ async def get_trade():
 
 # 查询持仓
 @app.get("/position")
-async def get_position():
+async def get_position(token: str = Depends(oauth2_scheme)):
     """"""
     tmp = rpc.get_position("aaa")
     return ("return: ", tmp)
@@ -196,7 +206,7 @@ async def get_position():
 
 # 查询账户
 @app.get("/account")
-async def get_account(vt_accountid = Query(...)):
+async def get_account(vt_accountid=Query(...), token: str = Depends(oauth2_scheme)):
     """"""
     tmp = rpc.get_account(vt_accountid)
     return ("return: ", tmp)
@@ -204,13 +214,15 @@ async def get_account(vt_accountid = Query(...)):
 
 # 查询合约
 @app.get("/contract")
-async def get_contract(vt_symbol = Query(...)):
+async def get_contract(vt_symbol=Query(...), token: str = Depends(oauth2_scheme)):
     """"""
     tmp = rpc.get_contract(vt_symbol)
     return ("return: ", tmp)
 
 
+# websocket传递数据
 _loop = ""
+
 
 @app.websocket("/ws/")
 async def websocket_endpoint(websocket: WebSocket):
@@ -221,16 +233,17 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             _loop = asyncio.get_event_loop()
-            data = await websocket.receive_text()
+            await websocket.receive_text()
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
 
-def calldata(topic: str, data:Any):
+def calldata(topic: str, data: Any):
     global DD, _loop, manager
     rpcdata = [topic, data]
     print(rpcdata)
     asyncio.run_coroutine_threadsafe(manager.broadcast(str(rpcdata)), _loop)
+
 
 rpc.callback = calldata
 
